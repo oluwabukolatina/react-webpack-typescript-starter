794cf09a3e5159c1e21502baaf1d26c48d33a9a2
oluwabukola tina <oluwabukolatina@gmail.com>
2021-06-23 19:21:56 +0100

    add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import '../App.css';
+import { Switch, Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+    </Switch>
+  );
 };
 export default App;
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <p className="footer"> &#169; IT Promocodes, {new Date().getFullYear()}</p>;
-};
-
-export default Footer;
