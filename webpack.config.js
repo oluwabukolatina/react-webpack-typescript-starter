29b733436b60407e7827b6aaff1ad51091a52ecc
oluwabukola tina <oluwabukolatina@gmail.com>
2021-06-23 22:06:40 +0100

    Configuring CSS for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 module.exports = {
-  entry: "./src/index.tsx",
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-  mode: process.env.NODE_ENV || "development",
+  entry: './src/index.tsx',
+  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+  mode: process.env.NODE_ENV || 'development',
   resolve: {
-    extensions: [".tsx", ".js"],
+    extensions: ['.tsx', '.js'],
   },
   devServer: {
-    contentBase: path.join(__dirname, "src"),
+    contentBase: path.join(__dirname, 'src'),
     historyApiFallback: true,
   },
   module: {
@@ -17,26 +18,28 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"],
+        use: ['ts-loader'],
       },
       {
         test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader", "css-modules-typescript-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, 'src', 'index.html'),
     }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
   ],
 };
