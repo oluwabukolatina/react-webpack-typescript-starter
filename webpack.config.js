342542e1583b080efeea7896af6d180d768d655a
oluwabukola tina <oluwabukolatina@gmail.com>
2021-06-23 21:42:04 +0100

    fix webpack react router issue

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ module.exports = {
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "development",
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".js"],
   },
-  devServer: { contentBase: path.join(__dirname, "src") },
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
